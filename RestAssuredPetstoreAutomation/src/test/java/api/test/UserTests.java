46ed7b546c2774c2a627b64e206781eecc1f6211
Your Name <you@example.com>
2024-09-20 19:54:42 +0530

    2nd day of api

--- RestAssuredPetstoreAutomation/src/test/java/api/test/UserTests.java
+++ RestAssuredPetstoreAutomation/src/test/java/api/test/UserTests.java
@@ -1,5 +1,7 @@
 package api.test;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -14,9 +16,10 @@ public class UserTests {
 	
 	Faker faker;
 	User userPayload;
+	public Logger logger;
 	
 	@BeforeClass
-	public void setupData()
+	public void setup()
 	{
 		faker=new Faker();
 		userPayload=new User();
@@ -28,15 +31,48 @@ public class UserTests {
 		userPayload.setEmail(faker.internet().safeEmailAddress());
 		userPayload.setPassword(faker.internet().password(5,10));
 		userPayload.setPhone(faker.phoneNumber().cellPhone());
+		//logs
 		
+		logger=LogManager.getLogger(this.getClass());
 	}
 	
 	@Test(priority=1)
 	public void testPostUser()
 	{
+		logger.info("-----creating user----");
 		Response response=UserEndpoints.createUser(userPayload);
 		response.then().log().all();
 		Assert.assertEquals(response.getStatusCode(), 200);
 	}
+	
+	@Test(priority=2)
+	public void testGetUserByname() {
+		
+		Response response=UserEndpoints.readUser(this.userPayload.getUsername());
+		response.then().log().all();
+		Assert.assertEquals(response.getStatusCode(), 200);
+	}
 
+	@Test(priority=3)
+	public void testUpdateByName() {
+		userPayload.setFirstName(faker.name().firstName());
+		userPayload.setLastName(faker.name().lastName());
+		userPayload.setEmail(faker.internet().safeEmailAddress());
+		Response response=UserEndpoints.updateUser(this.userPayload.getUsername(), userPayload);
+		response.then().log().body().statusCode(200);   //one more type to valiudate status code instead of assertion
+		
+		//checking if response is updated so for that will again call read user
+		
+		Response updatedresponse=UserEndpoints.readUser(this.userPayload.getUsername());
+		response.then().log().all();
+		Assert.assertEquals(updatedresponse.getStatusCode(), 200);
+	}
+	
+	@Test(priority=4)
+	public void testDeleteUserByname() {
+		
+		Response response=UserEndpoints.deleteUser(this.userPayload.getUsername());
+		response.then().log().all();
+		Assert.assertEquals(response.getStatusCode(), 200);
+	}
 }
